5bfa3d4364311c10f90f9f264c831a5ee3221475
Bankeybiharidassa <bankey.biharidassa@gmail.com>
2026-04-29 18:58:35 +0100

    Remove remaining .NET compile workflow references

--- build/tool.ps1
+++
@@ -1,5 +0,0 @@
-$PSScriptFilePath = Get-Item $MyInvocation.MyCommand.Path
-$RepoRoot = $PSScriptFilePath.Directory.Parent.FullName
-& dotnet pack $RepoRoot\src\main\wacs.csproj -c "Release"
-& dotnet tool uninstall simple-acme --global
-& dotnet tool install --global --add-source $RepoRoot\src\main\nupkg\ simple-acme --version 2.1.0
