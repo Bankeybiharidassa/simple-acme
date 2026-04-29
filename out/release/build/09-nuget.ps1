84584ce542abf9a87dc1fc21fe226aaa233db999
Bankeybiharidassa <bankey.biharidassa@gmail.com>
2026-04-29 21:40:53 +0100

    Regenerate clean allowlisted release bundle contents

--- out/release/build/09-nuget.ps1
+++
@@ -1,9 +0,0 @@
-# Publish NuGet
-
-if ([string]::IsNullOrWhiteSpace($env:NugetApiKey)) {
-	$env:NugetApiKey = Read-Host "Nuget API key"
-}
-
-Get-ChildItem $Final | Where-Object { $_.Extension -eq ".nupkg" } | Foreach-Object {
-   dotnet nuget push $_.FullName --source https://api.nuget.org/v3/index.json --skip-duplicate --api-key $env:NugetApiKey
-}
